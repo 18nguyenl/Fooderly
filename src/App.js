4cc15e84ce042b6d80cbc1f8e58d27921445170f
Ivan <ijaimes2872@gmail.com>
2018-10-20 18:12:46 -0500

    Added dir

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import AppMap from './components/AppMap.js'
+import Footer from './components/Footer'
+
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <AppMap></AppMap>
+        
+        <Footer></Footer>
+        
       </div>
+      // <div className="App">
+      //   <header className="App-header">
+      //     <img src={logo} className="App-logo" alt="logo" />
+      //     <a
+      //       className="App-link"
+      //       href="https://reactjs.org"
+      //       target="_blank"
+      //       rel="noopener noreferrer"
+      //     >
+      //       Learn React
+      //     </a>
+      //   </header>
+      // </div>
     );
   }
 }
---
+++ src/components/Footer/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+
+class Footer extends Component {
+  render() {
+    return (
+      <div>
+        <h3>----------</h3>
+        <div>Footer</div>
+      </div>
+    );
+  }
+}
+
+export default Footer;
